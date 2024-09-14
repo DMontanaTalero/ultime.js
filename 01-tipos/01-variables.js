a3a1c9b4c205434aa0aadd75e29d5a253754b2a6
danielito96 <daniel_m9603@hotmail.com>
2024-09-13 19:29:41 -0500

    Initial commit

---
+++ 01-tipos/01-variables.js
@@ -0,0 +1,11 @@
+//UpperCamelCase, camelCase, snake_case
+let nombre = "Hola mundo variables";
+let NombreCompleto;
+let nombreCompleto;
+let nombre_completo;
+
+let apellido;
+apellido = "Montaña";
+
+
+console.log(nombre);
