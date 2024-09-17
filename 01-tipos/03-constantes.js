b8220cda0c5bb65400936e2a185727e8f26b0469
danielito96 <daniel_m9603@hotmail.com>
2024-09-16 21:48:31 -0500

    03 - 05

---
+++ 01-tipos/03-constantes.js
@@ -0,0 +1,4 @@
+const nombre = "Hola mundo constantes";
+nombre = "Chanchito";
+
+console.log(nombre);
---
+++ 01-tipos/05-comentarios.js
@@ -0,0 +1,17 @@
+//asdfsdfasdf
+
+/*
+Todo lo que escribimos entremedio
+de los slash y los asteriscos
+va a ser ignorado
+*/
+
+/**
+ * Este es un
+ * comentario
+ * de varias
+ * lineas
+ */
+let numero = 42;
+
+console.log(numero)
