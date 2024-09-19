1f580cc8eeed1398ed0bdfaa36a53eedcb95b3e5
danielito96 <daniel_m9603@hotmail.com>
2024-09-18 21:58:40 -0500

    Operadores 01- 08
    Explicación y ejemplos de todos los operadores que se manejan en JS

---
+++ 02-operadores/01-aritmeticos.js
@@ -0,0 +1,18 @@
+let a = 5;
+let b = 7;
+
+// console.log(a + b, 'adicion'); //Adicion
+// console.log(a - b, 'resta'); //resta
+// console.log(a * b, 'multiplicación'); //multiplicacion
+// console.log(a / b, 'División'); //división
+// console.log(a % b, 'modulo'); //Módulo (Lo que sobra de una división)
+// console.log(a ** b, 'potencia'); //potencia
+
+//incrementar
+console.log(++a);
+console.log(a++);
+console.log(a);
+//decrementar
+console.log(--a);
+console.log(a--);
+console.log(a);
---
+++ 02-operadores/07-ordenOperadores.js
@@ -0,0 +1,8 @@
+let resultado = 8/(2*(2+2));
+
+// Parentesis, división, multiplicación, suma, resta
+
+console.log(resultado);
+console.log(5/2*4);
+console.log((5/2)*4);
+console.log(5/(2*4));
